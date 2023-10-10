3640c7fb8d1650742e3ffeee4a1846a913c7ff70
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-09 21:30:21 -0400

    return the number of letters

--- While02.py
+++ While02.py
@@ -1,3 +1,4 @@
+import string
 def main(s):
     """
     A variable of type str is given. Find how many letters it contains and return.
@@ -6,4 +7,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    letters=0
+    while i<len(s):
+        if s[i].isalpha():
+            letters+=1
+        i+=1 
+    return "Number of letters:",letters
+print(main( "Python 2022"))
+print(main("e324xE"))
