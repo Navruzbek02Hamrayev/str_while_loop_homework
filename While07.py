0da767a121b7d32e71489329b9abd9bad02c6c7f
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-09 22:40:22 -0400

    return the even digits

--- While07.py
+++ While07.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    even=0
+    while i<len(s):
+        if int(s[i])%2==0:
+            even+=1
+        i+=1
+    return even
+print(main("56786543250"))
+print(main("123456"))
